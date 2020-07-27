22bcd4abd7574cc5dcfc5663a3fc94c35cde9744
WITOLD BARCINSKI <wbarcinski@hotmail.com>
2020-07-27 18:05:25 -0500

    Add project files.

---
+++ DictionaryIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
